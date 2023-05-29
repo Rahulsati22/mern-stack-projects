6fbfdc36012fc982a30c158dfa88a32e400db901
Rahulsati22 <97585225+Rahulsati22@users.noreply.github.com>
2023-05-29 22:55:36 +0530

    Update index.js

--- chatssss/frontend/src/index.js
+++ chatssss/frontend/src/index.js
@@ -5,15 +5,12 @@ import { Provider } from 'react-redux';
 import Store from './store'
 import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
-import ChatProvider from './Context/ChatProvider'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={Store}>
     <BrowserRouter>
       <ChakraProvider>
-        <ChatProvider>
           <App />
-        </ChatProvider>
       </ChakraProvider>
     </BrowserRouter>
   </Provider>
